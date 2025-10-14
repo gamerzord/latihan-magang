36a5159d02fbe975c94588a33f0dd28cd2043b39
gamerzord <gamerzord76@gmail.com>
2025-10-14 18:08:30 +0700

    modern one lol

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,13 +1,11 @@
 export default defineNuxtRouteMiddleware((to) => {
   if (process.client) {
     const navigationEntry = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming
-    
-    if (navigationEntry && (navigationEntry.type === 'reload' || navigationEntry.type === 'navigate')) {
-      if (performance.navigation?.type === 1) { // TYPE_RELOAD
-        localStorage.clear()
-        useCookie('auth_token').value = null
-        console.log('Local storage cleared on page reload!')
-      }
+   
+    if (navigationEntry && navigationEntry.type === 'reload') {
+      localStorage.clear()
+      useCookie('auth_token').value = null
+      console.log('Local storage cleared on page reload!')
     }
   }
 
